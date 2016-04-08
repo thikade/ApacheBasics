79d80a4762bb464ac4a67c48798a59c2b17cea97
root <root@centos6.(none)>
2016-04-08 04:38:08 -0400

    added packages

--- installPackages.sh
+++ installPackages.sh
@@ -4,7 +4,7 @@
 # PACKAGE INSTALLATION
 ###########################
 yum -y -q update
-yum -y -q install httpd man telnet nc strace lsof git links
+yum -y -q install httpd man telnet nc strace curl wget lsof git links bind-utils wireshark
 yum -y -q install epel-release.noarch 
 yum -y -q install bash-completion
 
