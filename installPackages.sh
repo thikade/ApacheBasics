cbd507616da7179a0eea3fc5bcf1c2ee56c2997b
thikade <root@centos6.(none)>
2016-04-08 16:05:34 +0200

    more packages

--- installPackages.sh
+++ installPackages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+P="httpd httpd-tools man telnet nc strace curl wget lsof git links bind-utils wireshark"
+P="$P yum-utils bash-completion"
+
 ###########################
 # PACKAGE INSTALLATION
 ###########################
 yum -y -q update
-yum -y -q install httpd man telnet nc strace curl wget lsof git links bind-utils wireshark
 yum -y -q install epel-release.noarch 
-yum -y -q install bash-completion
-
+yum -y -q install $P
+echo ""
+echo "yum installed: $P"
