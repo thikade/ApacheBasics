dad87666722d62178cd5e4944152c41ab2edced5
thikade <thomas.hikade@gmail.com>
2016-04-10 12:31:37 +0200

    backup of instances done as dir copy

--- setupTraining.sh
+++ setupTraining.sh
@@ -35,7 +35,8 @@ fi
 echo ""
 
 if [ -d $EXAMPLE_DIR ]; then
-  sudo su - $USER -c "mkdir $EXAMPLE_BACKUP_DIR 2>/dev/null; tar czf $EXAMPLE_BACKUP_DIR/apache_`date +%Y%m%d_%H.%M.%S`.tgz -C $USER_HOME apache "
+  # sudo su - $USER -c "mkdir $EXAMPLE_BACKUP_DIR 2>/dev/null; tar czf $EXAMPLE_BACKUP_DIR/apache_`date +%Y%m%d_%H.%M.%S`.tgz -C $USER_HOME apache "
+  sudo su - $USER -c "mkdir $EXAMPLE_BACKUP_DIR 2>/dev/null; cp -rf $EXAMPLE_DIR $EXAMPLE_BACKUP_DIR/apache_`date +%Y%m%d_%H.%M.%S`"
 fi
 sudo su - $USER -c "mkdir $EXAMPLE_DIR  2> /dev/null"
 sudo su - $USER -c "mkdir bin 2> /dev/null"
